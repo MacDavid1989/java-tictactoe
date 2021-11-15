9c70624bdb5e4a47565acf3fea97879e7ac07e40
David <62397679+MacDavid1989@users.noreply.github.com>
2021-11-15 15:30:07 -0500

    Extend Pieces to inherit Player
    When constructing Pieces, calling super will call the extended class and allow usage of the constructor to set name

--- Pieces.java
+++ Pieces.java
@@ -1,6 +1,13 @@
 /**
  * Pieces
  */
-public class Pieces {
-  
+public class Pieces extends Player {
+  private final int row;
+  private final int col;
+
+  public Pieces(char name, int row, int col) {
+    super(name);
+    this.row = row;
+    this.col = col;
+  }
 }

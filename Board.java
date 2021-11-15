8329d81456291639f04c23439c48939a8fdc0b08
David <62397679+MacDavid1989@users.noreply.github.com>
2021-11-15 15:11:04 -0500

    Create getters
    Create getter methods to allow other objects to read what the value of the instance variables are

--- Board.java
+++ Board.java
@@ -5,4 +5,13 @@ public class Board
 {
   private final int MAX_ROWS;
   private final int MAX_COLS;
+
+  public int getMAXROWS() 
+  {
+    return MAX_ROWS;
+  }
+  public int getMAXCOLS() 
+  {
+    return MAX_COLS;
+  }
 }

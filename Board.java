038a57799b5be1478ef1ed0111659e884345d619
David <62397679+MacDavid1989@users.noreply.github.com>
2021-11-15 15:20:13 -0500

    Setup constructor
    Setup Board constructor to instantiate a new gameboard object from Pieces using max_rows and max_cols

--- Board.java
+++ Board.java
@@ -1,17 +1,22 @@
 /**
  * Board
  */
-public class Board 
-{
+public class Board {
   private final int MAX_ROWS;
   private final int MAX_COLS;
+  private Pieces[][] gameboard;
+  
+  public Board(int max_rows, int max_cols) {
+    this.MAX_ROWS = max_rows;
+    this.MAX_COLS = max_cols;
 
-  public int getMAXROWS() 
-  {
+    gameboard = new Pieces[MAX_ROWS][MAX_COLS];
+  }
+
+  public int getMAXROWS() {
     return MAX_ROWS;
   }
-  public int getMAXCOLS() 
-  {
+  public int getMAXCOLS() {
     return MAX_COLS;
   }
 }
